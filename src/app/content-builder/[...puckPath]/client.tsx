6993c5c6468a3bde9d9298eb1d8310340105b6a9
spanandequipment <ilya.i@skinact.com>
2025-05-09 16:22:55 -0700

    action buttons

--- src/app/content-builder/[...puckPath]/client.tsx
+++ src/app/content-builder/[...puckPath]/client.tsx
@@ -1,19 +1,68 @@
 'use client'
 
 import type { Data } from '@measured/puck'
-import { Puck } from '@measured/puck'
+import { Puck, usePuck } from '@measured/puck'
 import config from '../../../../puck.config'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 export function Client({ id, path, data }: { id: string; path: string; data: Partial<Data> }) {
+  const router = useRouter()
+  const [isSaving, setIsSaving] = useState(false)
+
+  const close = () => {
+    router.push(`/admin/collections/pages/${id}`)
+  }
+
+  const saveAndClose = async (data: Data) => {
+    await save(data)
+    close()
+  }
+
+  const save = async (data: Data) => {
+    setIsSaving(true)
+
+    await fetch('api', {
+      method: 'post',
+      body: JSON.stringify({ id, path, data }),
+    })
+
+    setIsSaving(false)
+  }
+
   return (
     <Puck
       config={config}
       data={data}
-      onPublish={async (data) => {
-        await fetch('api', {
-          method: 'post',
-          body: JSON.stringify({ id, path, data }),
-        })
+      overrides={{
+        headerActions: () => {
+          const { appState } = usePuck()
+          return (
+            <>
+              <button className="button button--secondary" onClick={close}>
+                CLOSE
+              </button>
+              <button
+                className="button button--secondary"
+                disabled={isSaving}
+                onClick={() => {
+                  saveAndClose(appState.data)
+                }}
+              >
+                SAVE & CLOSE
+              </button>
+              <button
+                className="button"
+                disabled={isSaving}
+                onClick={() => {
+                  save(appState.data)
+                }}
+              >
+                {isSaving ? 'SAVING...' : 'SAVE'}
+              </button>
+            </>
+          )
+        },
       }}
     />
   )
